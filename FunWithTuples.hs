b84c76bc5f4fa68034d31efabe2e3823c856ec12
amit dash <dbamit@thoughtworks.com>
2017-05-28 23:37:48 +0530

    Initial commit

---
+++ FunWithTuples.hs
@@ -0,0 +1,13 @@
+module FunWithTuples where
+
+data TupleList = TupleStr [(String,String)] | TupleInt [(Integer,Integer)] | TupleDouble deriving (Show, Eq)
+data IntStr = Int Integer | Str String | NotSupported deriving (Show, Eq)
+
+mapTuples :: Show a => (a -> a -> a) -> [(a, a)] -> [a]
+mapTuples f ts = map (\t -> f (fst t) (snd t)) ts
+
+reduceTupleList :: TupleList  -> IntStr
+reduceTupleList ts = case ts of
+ TupleStr s -> Str (foldr (++) "" (mapTuples (++) s))
+ TupleInt i -> Int (foldr (+) 0 (mapTuples (-) i))
+ _ -> NotSupported
