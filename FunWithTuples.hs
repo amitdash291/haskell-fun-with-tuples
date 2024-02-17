c171e2f7259ef6948f605b9d661b6897994000f6
Amit Dash <rockin291@gmail.com>
2024-02-18 01:13:58 +0530

    Add more examples for usage

--- FunWithTuples.hs
+++ FunWithTuples.hs
@@ -1,15 +1,25 @@
 module FunWithTuples where
 
-data TupleList = TupleStr [(String,String)] | TupleInt [(Integer,Integer)] | TupleDouble deriving (Show, Eq)
-data IntStr = Int Integer | Str String | NotSupported deriving (Show, Eq)
+data Tuples = IntTuples [(Int,Int)] | StrTuples [(String,String)] | DoubleTuples[(Double, Double)] deriving (Show, Eq)
+data NumStr = Num Int | Str String | NotSupported deriving (Show, Eq)
 
 mapTuples :: Show a => (a -> a -> a) -> [(a, a)] -> [a]
 mapTuples f ts = map (\t -> f (fst t) (snd t)) ts
 
-reduceTupleList :: TupleList  -> IntStr
-reduceTupleList ts = case ts of
- TupleStr s -> Str (foldr (++) "" (mapTuples (++) s))
- TupleInt i -> Int (foldr (+) 0 (mapTuples (-) i))
- _ -> NotSupported
+mapStrTuples :: [(String, String)] -> [String]
+mapStrTuples ts = intersperse ", " (mapTuples (\a b -> a ++ " " ++ b) ts)
+
+reduceTuples :: Tuples  -> NumStr
+reduceTuples t = case t of
+  IntTuples tn -> Num (foldr (+) 0 (mapTuples (+) tn))
+  StrTuples ts -> Str (foldr (++) "" (mapStrTuples ts))
+  _ -> NotSupported
  
- -- Example usage: tupleList = TupleInt [(2,1), (5,1)]; reduceTupleList tupleList (output: 5)
+ -- Example usage
+ main = do
+  let intTuples = IntTuples [(2,1), (5,1)]
+  putStrLn $ show $ reduceTuples intTuples -- output: Num 9
+  let strTuples = StrTuples [("John","Doe"), ("Jane","Doe")]
+  putStrLn $ show $ reduceTuples strTuples -- output: Str "John Doe, Jane Doe"
+  let doubleTuples = DoubleTuples [(1.5,1.8), (2.2,1.4)]
+  putStrLn $ show $ reduceTuples doubleTuples -- output: NotSupported
